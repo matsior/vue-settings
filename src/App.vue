931372737932e36dca3df19c2ea62711b5b40f23
Mateusz Krajewski <maatsior@gmail.com>
2025-10-26 16:15:14 +0100

    add TabLink component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Tab, TabKey } from '@/types.ts';
+import { ref } from 'vue';
+import TabLink from '@/components/TabLink.vue';
+
+const tabs: Tab[] = [
+  {
+    key: 'General',
+    label: 'General',
+  },
+  {
+    key: 'Notifications',
+    label: 'Notifications',
+  },
+  {
+    key: 'Privacy',
+    label: 'Privacy',
+  },
+];
+
+const currentTab = ref<TabKey>('General');
+</script>
 
 <template>
-  <main>Hello</main>
+  <main class="max-w-2xl mx-auto px-4">
+    <nav class="font-medium text-center text-gray-500 border-b border-gray-200 mb-4">
+      <ul class="flex flex-wrap -mb-px">
+        <li v-for="tab in tabs" :key="tab.key">
+          <TabLink :tab="tab" :current-tab="currentTab" @click="currentTab = tab.key" />
+        </li>
+      </ul>
+    </nav>
+  </main>
 </template>
