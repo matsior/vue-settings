2541662c1b968c25d619fcccc6ffe2e8c5475e7f
Mateusz Krajewski <maatsior@gmail.com>
2025-11-02 19:31:46 +0100

    add generics in useSettings

--- src/composables/useSettings.ts
+++ src/composables/useSettings.ts
@@ -1,4 +1,12 @@
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+
+interface SettingsMap {
+  general: GeneralSettings;
+  privacy: PrivacySettings;
+  notifications: NotificationsSettings;
+}
+
+type SettingsKey = keyof SettingsMap;
 
 interface GeneralSettings {
   username: string;
@@ -8,23 +16,43 @@ interface GeneralSettings {
   country: string;
 }
 
-const general = ref<GeneralSettings>({
-  username: '',
-  email: '',
-  about: '',
-  gender: 'male',
-  country: 'USA',
-});
+const init = <T extends SettingsKey>(key: T, defaults: SettingsMap[T]) => {
+  const stored = localStorage.getItem(key);
+
+  return stored !== null ? JSON.parse(stored) : defaults;
+};
+
+const watcher =
+  <T extends SettingsKey>(key: T) =>
+  (value: SettingsMap[T]) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  };
+
+const general = ref<GeneralSettings>(
+  init('general', {
+    username: '',
+    email: '',
+    about: '',
+    gender: 'male',
+    country: 'USA',
+  }),
+);
+
+watch(general, watcher('general'), { deep: true });
 
 interface NotificationsSettings {
   email: boolean;
   sms: boolean;
 }
 
-const notifications = ref<NotificationsSettings>({
-  email: false,
-  sms: false,
-});
+const notifications = ref<NotificationsSettings>(
+  init('notifications', {
+    email: false,
+    sms: false,
+  }),
+);
+
+watch(notifications, watcher('notifications'), { deep: true });
 
 interface PrivacySettings {
   visibility: Visibility;
@@ -33,10 +61,14 @@ interface PrivacySettings {
 
 type Visibility = 'public' | 'private';
 
-const privacy = ref<PrivacySettings>({
-  visibility: 'public',
-  searchEngineIndexing: false,
-});
+const privacy = ref<PrivacySettings>(
+  init('privacy', {
+    visibility: 'public',
+    searchEngineIndexing: false,
+  }),
+);
+
+watch(privacy, watcher('privacy'), { deep: true });
 
 export function useSettings() {
   return {
